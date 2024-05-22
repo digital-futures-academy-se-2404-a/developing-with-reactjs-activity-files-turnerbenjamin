2895076affd6d4b546e8d3cf0c43ac50e19875b0
Ben Turner <turner.benjamin88@gmail.com>
2024-05-22 16:25:24 +0100

    COMPLETED: Activity 3

---
+++ activity-files/IntroductionToReactJS/vite-project/src/components/AnotherComponent.jsx
@@ -0,0 +1,15 @@
+const AnotherComponent = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        commodi!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad, nesciunt!
+      </p>
+    </>
+  );
+};
+
+export default AnotherComponent;
