5178a08d072cb8057dc201b20d2281a4184fb8ce
Ben Turner <turner.benjamin88@gmail.com>
2024-05-22 16:30:53 +0100

    COMPLETED: Activity 4

--- activity-files/IntroductionToReactJS/vite-project/src/App.jsx
+++ activity-files/IntroductionToReactJS/vite-project/src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
-import AnotherComponent from "./components/AnotherComponent";
 
 function App() {
-  return (
-    <>
-      <MyComponent />
-      <AnotherComponent />
-    </>
-  );
+  return <MyComponent />;
 }
 
 export default App;
---
+++ activity-files/IntroductionToReactJS/vite-project/src/components/MyClassComponent.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class MyClassComponent extends Component {
+  render() {
+    return (
+      <>
+        <h2>Class Component</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
+          eligendi.
+        </p>
+      </>
+    );
+  }
+}
